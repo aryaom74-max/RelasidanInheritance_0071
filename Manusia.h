a2aff2202fd81b4fdd7187e38993805e3e53da14
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-12 18:18:58 +0700

    Update Manusia.h

--- Manusia.h
+++ Manusia.h
@@ -5,3 +5,19 @@
 #include <string>
 #include "jantung.h" // Wajib disertakan karena ada variabel varJantung
 
+class manusia {
+public:
+    std::string name;
+    jantung varJantung; // Objek jantung sebagai bagian integral dari manusia
+
+    manusia(std::string pName) : name(pName) {
+        std::cout << name << " hidup\n";
+    }
+
+    ~manusia() {
+        std::cout << name << " mati\n";
+    }
+};
+
+#endif
+
