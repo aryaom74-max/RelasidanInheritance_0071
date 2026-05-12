193c5c467a5788ec34c457752ba0f3dff5a15b43
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-12 18:20:46 +0700

    Update ibu.h

--- ibu.h
+++ ibu.h
@@ -24,3 +24,51 @@ public:
 
 #endif
 
+#ifndef IBU_H
+#define IBU_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Forward Declaration: Memberitahu kompiler bahwa class anak akan ada nanti
+class anak; 
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
+// Catatan: Implementasi fungsi di bawah ini sebaiknya diletakkan di file .cpp 
+// atau pastikan class anak sudah didefinisikan lengkap sebelum fungsi ini dipanggil.
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        // Kita butuh definisi lengkap class anak untuk mengakses member .nama
+        // Jika kode ini di file header, pastikan sudah #include "anak.h"
+        cout << daftar_anak[i]->nama << endl; 
+    }
+    cout << endl;
+}
+
+#endif
+
