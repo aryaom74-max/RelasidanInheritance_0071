9d595f29265ac8a3223bfa6f73803cadeb3c397b
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-12 18:17:40 +0700

    Update Jantung.h

--- Jantung.h
+++ Jantung.h
@@ -1,3 +1,16 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
 
+#include <iostream> // Library wajib untuk cout
+
+class jantung {
+public:
+    jantung() {
+        std::cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        std::cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
