34d7fd5ebe02bce1ebe3fbc39920cfa50c009b8f
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-12 18:20:35 +0700

    Update ibu.h

--- ibu.h
+++ ibu.h
@@ -1,3 +1,26 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+#include <iostream> // Dibutuhkan untuk cout
+#include <string>   // Dibutuhkan untuk string
+
+using namespace std; // Agar tidak perlu menulis std::string atau std::cout
+
+class anak {
+public:
+
+    string nama;
+    
+    // Constructor
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+    
+    // Destructor
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif
+
