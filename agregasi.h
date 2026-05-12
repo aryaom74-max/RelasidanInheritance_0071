cdd6f381f0e827bf382a3d0f09bd3836af5f4715
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-12 18:19:17 +0700

    Update agregasi.h

--- agregasi.h
+++ agregasi.h
@@ -8,34 +8,3 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main() {
-    // Membuat objek secara dinamis di heap menggunakan 'new'
-    ibu* varIbu = new ibu("dini");
-    ibu* varIbu2 = new ibu("novi");
-    
-    anak* varAnak1 = new anak("tono");
-    anak* varAnak2 = new anak("rini");
-    anak* varAnak3 = new anak("Dewi");
-
-    // Menghubungkan objek (Agregasi)
-    varIbu->tambahAnak(varAnak1);
-    varIbu->tambahAnak(varAnak2);
-    varIbu2->tambahAnak(varAnak3);
-    varIbu2->tambahAnak(varAnak1); // Satu anak bisa dimiliki dua ibu dalam agregasi ini
-
-    // Menampilkan data
-    varIbu->cetakAnak();
-    varIbu2->cetakAnak();
-
-    // Penghapusan objek
-    // Dalam Agregasi, menghapus 'ibu' tidak otomatis menghapus 'anak'
-    delete varIbu;
-    delete varIbu2;
-    
-    // Objek anak harus dihapus manual karena dibuat dengan 'new'
-    delete varAnak1;
-    delete varAnak2;
-    delete varAnak3;
-
-    return 0;
-}
